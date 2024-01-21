bbbce24ed757aab9bfb4a523fe13fac559a53ce9
João Vitor <joao25072005@gmail.com>
2024-01-21 17:33:49 -0300

    Exercicio 2 utilizando constructor na classe

--- index.js
+++ index.js
@@ -23,3 +23,27 @@ const corolla = new Carros();
   Kilômetros = 30.2 // Corolla faz em média 30,2km/l
 
   corolla.gasto_medio() // Saída: 30,2 / 5,63 => R$ 5,36
+
+// 2) Crie uma classe para representar pessoas.
+// Para cada pessoa teremos os atributos nome, peso e altura.
+// As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura));
+// Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer o valor
+// do seu IMC;
+
+class Pessoas {
+  nome;
+  peso;
+  altura;
+
+  constructor (nome, peso, altura) {
+    this.nome = nome;
+    this.peso = peso;
+    this.altura = altura;
+  }
+  mensagem_IMC() {
+    console.log(`Meu nome é ${this.nome} e meu IMC é: ` + (this.peso / (this.altura ** 2)).toFixed(2))
+  }
+}
+
+const José = new Pessoas('José', 70, 1.75)
+  José.mensagem_IMC()
